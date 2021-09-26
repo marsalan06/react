8faffe1ac4e78b2489645abaf5f8d5b85e78742d
marsalan06 <arsalan.9798@gmail.com>
2021-09-26 20:29:07 +0500

    filtering

--- src/App.js
+++ src/App.js
@@ -23,17 +23,49 @@ function App(){
    console.log(data); 
  }
 
+ const filterData = (data) =>{
+   const filteredData = [];
+   
+   if (!filters.name){
+     return data;
+   }
+
+   // we'll first eleminate the default values, incase of empty form
+   for (const item of data){
+     //filters is the name of the state being saved on search button being pressed
+    if ( filters.name !== "" && item.name !== filters.name){
+      continue;
+    }
+    if ( filters.price !== 0 && item.price > filters.price){
+      continue;
+    }
+    if ( filters.type !== "" && item.type !== filters.type){
+      continue;
+    }
+    if ( filters.brand !== "" && item.brand !== filters.brand){
+      continue;
+    }
+     filteredData.push(item)
+   } 
+
+   return filteredData
+ }
+
  return( <div className = "App">
     <img src={logo} className="App-logo" alt="logo" />
     <SearchBar updateSearchParams = {updateFilters}/>
     <AddItem addItem = {addItemToData}/>
-    <ItemsDisplay items = {data["items"]} />
+
+    {/* passed the array of data to be display through the filter */}
+    <ItemsDisplay items = {filterData(data["items"])} />
+    
     {/* <p>From callback</p>
     <p>Name: {"name" in data ? data["name"] : "No data to display"}</p>
     <p>Max Price: {"price" in data ? data["price"] : "No data to display"}</p>
     <p>Type: {"type" in data ? data["type"] : "No data to display"}</p>
     <p>Brand: {"brand" in data ? data["brand"] : "No data to display"}</p>
      A13*/ } 
+  
   </div>
   );
 }
